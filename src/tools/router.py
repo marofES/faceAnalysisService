49eff108cf33321b5fed7bac5a972f432f8cee8c
Marof Mia <marof.mia@accelx.net>
2024-07-16 17:34:21 +0600

    Add new API that cropped detected faces

--- src/tools/router.py
+++ src/tools/router.py
@@ -10,7 +10,7 @@ from model_load import UltraFaceTF
 from src.auth import schemas, service, dependencies
 from src.database import get_session
 from src.auth.constants import RoleEnum
-from src.tools.utils import resize_with_envelope
+from src.tools.utils import *
 import base64
 
 model_path = "D:/faceService/export_models/slim"
@@ -43,18 +43,57 @@ async def face_detect(face_image: UploadFile = File(...)):
         #print(f'results: {results}')
 
         for i, result in enumerate(results):
-            start_x = int(result[2] * w)
-            start_y = int(result[3] * h)
-            end_x = int(result[4] * w)
-            end_y = int(result[5] * h)
+            start_x, start_y, end_x, end_y = get_coordinates(result, w, h)
 
             cv2.rectangle(img, (start_x, start_y), (end_x, end_y), (0, 255, 0), 2)
-            resized_img = resize_with_envelope(img, (768, 1024))
+        resized_img = resize_with_envelope(img, (768, 1024))
 
-        # Convert the processed image to base64
-        _, buffer = cv2.imencode('.jpg', resized_img)
-        img_base64 = base64.b64encode(buffer).decode('utf-8')
+        img_base64 = image_to_base64(resized_img)
         
         return JSONResponse(content={"face_image": face_image.filename, "base64_image": img_base64})
+    else:
+        return {'error': 'TF model is not loading'}
+
+
+@router.post("/face-detection-and-spilt/")
+async def face_detection_and_spilt(face_image: UploadFile = File(...)):
+    # Read the uploaded file
+    contents = await face_image.read()
+    if contents is None:
+        return {'error': 'Image not uploaded'}
+    # Convert file contents to a NumPy array for OpenCV
+    np_arr = np.frombuffer(contents, np.uint8)
+    img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+    
+    # Process the image
+    h, w, _ = img.shape
+    img_resize = cv2.resize(img, (320, 240))
+    img_resize = cv2.cvtColor(img_resize, cv2.COLOR_BGR2RGB)
+    img_resize = img_resize - 127.0
+    img_resize = img_resize / 128.0
+
+    
+    
+    if model_loading is not None:
+        results = model_loading.predict(np.expand_dims(img_resize, axis=0))  # result=[background,face,x1,y1,x2,y2]
+        #print(f'results: {results}')
+        cropped_img_list = []
+        padding = 5
+
+        for i, result in enumerate(results, 1):
+            start_x, start_y, end_x, end_y = get_coordinates(result, w, h)
+
+            cropped_img = crop_with_padding(img, start_x, start_y, end_x, end_y, w, h, padding)
+
+            cropped_image_resize = resize_with_envelope(cropped_img, (256, 256))
+            cr_base_64 = image_to_base64(cropped_image_resize)
+            cropped_img_list.append(cr_base_64)
+            cv2.rectangle(img, (start_x, start_y), (end_x, end_y), (0, 255, 0), 2)
+        
+        resized_img = resize_with_envelope(img, (768, 1024))
+
+        original_img = image_to_base64(resized_img)
+        
+        return JSONResponse(content={"original_image": original_img,"total_face_count": i, "cropped_image_list": cropped_img_list})
     else:
         return {'error': 'TF model is not loading'}
--- src/tools/utils.py
+++ src/tools/utils.py
@@ -1,6 +1,7 @@
 # any other non-business logic functions e.g. response normalization, data enrichment, etc.
 import cv2
 import numpy as np
+import base64
 
 def resize_with_envelope(image, target_size=(768, 1024)):
     h, w = image.shape[:2]
@@ -32,3 +33,25 @@ def resize_with_envelope(image, target_size=(768, 1024)):
     canvas[y_offset:y_offset+new_h, x_offset:x_offset+new_w] = resized_image
 
     return canvas
+
+
+def image_to_base64(image):
+    # Convert the processed image to base64
+    _, buffer = cv2.imencode('.jpg', image)
+    return base64.b64encode(buffer).decode('utf-8')
+
+
+def get_coordinates(result, w, h):
+    start_x = int(result[2] * w)
+    start_y = int(result[3] * h)
+    end_x = int(result[4] * w)
+    end_y = int(result[5] * h)
+    return start_x, start_y, end_x, end_y
+
+
+def crop_with_padding(img, start_x, start_y, end_x, end_y, w, h, padding=5):
+    ap_start_x = max(0, start_x - padding)
+    ap_start_y = max(0, start_y - padding)
+    ap_end_x = min(w, end_x + padding)
+    ap_end_y = min(h, end_y + padding)
+    return img[ap_start_y:ap_end_y, ap_start_x:ap_end_x]
